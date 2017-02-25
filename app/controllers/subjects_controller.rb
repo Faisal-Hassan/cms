66bfebc2d8bb6e5ac4bae05eaafc6f67afbe66cc
Faisal Hassan <faisal.hassanx@gmail.com>
2017-02-25 00:54:11 +0000

    Add whitelist params to subject controller

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -13,6 +13,16 @@ class SubjectsController < ApplicationController
   end
   
   def create
+    # Instantiate a new object using form parameters
+    @subject= Subject.new(subject_params)
+    # Save the object
+    if @subject.save
+    # If save succeeds, redirect to the index action
+      redirect_to(subjects_path)
+    else
+    # If save fails, redisplay the form so user can fix problems
+      render('new')
+    end
   end
 
   def edit
@@ -27,4 +37,9 @@ class SubjectsController < ApplicationController
   def destroy
   end
   
+  private
+  
+  def subject_params
+    params.require(:subject).permit(:name, :position, :visible)
+  end
 end
