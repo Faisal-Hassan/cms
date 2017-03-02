b7caf8979e3c0f98c5ba776ac711c000642192d4
Faisal Hassan <faisal.hassanx@gmail.com>
2017-03-02 03:35:01 +0000

    Add new view for section controller

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -13,9 +13,9 @@ class SectionsController < ApplicationController
   end
   
   def create
-    @section - Section.new(section_params)
-    if section.save
-      flash[:notice] = "Sections has been created successfully"
+    @section = Section.new(section_params)
+    if @section.save
+      flash[:notice] = "Section has been created successfully"
       redirect_to(sections_path)
     else
       render('new')
