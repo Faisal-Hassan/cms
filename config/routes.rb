c651177f544c96e8b53f5717e3516acfa8e070e1
Faisal Hassan <faisal.hassanx@gmail.com>
2017-03-02 01:22:37 +0000

    Destroy demo controller

--- config/routes.rb
+++ config/routes.rb
@@ -24,19 +24,10 @@ Rails.application.routes.draw do
     
   end
 
-  get 'home/about'
-
   get 'home/contact'
   
-  get 'demo/lynda'
+  root 'home#about'
   
-  get 'demo/hi'
-  
-  get 'demo/hello'
-
-  get 'demo/index'
-  
-  root 'demo#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
