4b596019d5ba49119b7eedc27d06c77e06c8748c
Faisal Hassan <faisal.hassanx@gmail.com>
2017-03-02 01:14:42 +0000

    Create actions for sections controller

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,16 +1,56 @@
 class SectionsController < ApplicationController
+  
   def index
+    @section = Sections.sorted
   end
 
   def show
+    @section = Section.find(params[:id])
   end
 
   def new
+    @section = Section.new
+  end
+  
+  def create
+    @section - Section.new(section_params)
+    if section.save
+      flash[:notice] = "Sections has been created successfully"
+      redirect_to(sections_path)
+    else
+      render('new')
+    end
   end
 
   def edit
+    @section = Setion.find(params[:id])
+  end
+  
+  def update
+    @section = Section.find(params[:id])
+    if @section.update_attributes(section_params)
+      flash[:notice] = "Section has been updated successfully"
+      redirect_to(section_path(@section))
+    else
+      render('edit')
+    end
   end
 
   def delete
+    @section = Section.find(params[:id])
+  end
+  
+  def destroy
+    @section = Section.find(params[:id])
+    @section.destroy
+    flash[:notice] = "Section has been deleted successfully"
+    redirect_to(sections_path)
+  end
+  
+  private
+  
+  def section_params
+    params.require(:section).permit(:page_id, :name, :position, 
+    :visible, :content_type, :content)
   end
 end
