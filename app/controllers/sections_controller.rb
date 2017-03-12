98954992104f1e22310ff0049c1606edc3e2ac0b
Faisal Hassan <faisal.hassanx@gmail.com>
2017-03-12 21:53:39 +0000

    Add before action filters to sections controller

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -2,18 +2,19 @@ class SectionsController < ApplicationController
   
   layout 'admin'
   
+  before_action :find_pages, :only => [:new, :create, :edit, :update]
+  before_action :find_section_by_id, :except => [:index, :new, :create]
+  before_action :set_section_count, :only => [:new, :create, :edit, :update]
+  
   def index
     @sections = Section.sorted
   end
 
   def show
-    @section = Section.find(params[:id])
   end
 
   def new
     @section = Section.new
-    @section_count = Section.count + 1
-    @pages = Page.sorted
   end
   
   def create
@@ -22,16 +23,11 @@ class SectionsController < ApplicationController
       flash[:notice] = "Section has been created successfully"
       redirect_to(sections_path)
     else
-      @section_count = Section.count + 1
-      @pages = Page.sorted
       render('new')
     end
   end
 
   def edit
-    @section = Section.find(params[:id])
-    @section_count = Section.count
-    @pages = Page.sorted
   end
   
   def update
@@ -40,18 +36,14 @@ class SectionsController < ApplicationController
       flash[:notice] = "Section has been updated successfully"
       redirect_to(section_path(@section))
     else
-      @section_count = Section.count
-      @pages = Page.sorted
       render('edit')
     end
   end
 
   def delete
-    @section = Section.find(params[:id])
   end
   
   def destroy
-    @section = Section.find(params[:id])
     @section.destroy
     flash[:notice] = "Section has been deleted successfully"
     redirect_to(sections_path)
@@ -63,4 +55,20 @@ class SectionsController < ApplicationController
     params.require(:section).permit(:page_id, :name, :position, 
     :visible, :content_type, :content)
   end
+  
+  def find_pages
+    @pages = Page.sorted
+  end
+  
+  def find_section_by_id
+    @section = Section.find(params[:id])
+  end
+  
+  def set_section_count
+    @section_count = Section.count
+    if params[:action] == 'new' || params[:action] == 'create'
+      @section_count += 1
+    end
+  end
+  
 end
