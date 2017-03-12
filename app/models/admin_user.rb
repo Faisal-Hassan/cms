e1b8b7e0050b347c16fc83f3fb89a9c66c03a5a3
Faisal Hassan <faisal.hassanx@gmail.com>
2017-03-12 21:34:44 +0000

    Add before action filters to pages controller

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -6,6 +6,7 @@ class AdminUser < ApplicationRecord
   has_many :sections, :through => :section_edits
   
   EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+  FORBIDDEN_USERNAMES = ['littlebopeep', 'humptydumpty', 'marymary']
   
   validates :first_name, :presence => true,
                          :length => { :maximum => 25 }
@@ -18,5 +19,16 @@ class AdminUser < ApplicationRecord
                          :length => { :maximum => 100 },
                          :format => { :with => EMAIL_REGEX },
                          :confirmation => true
+                         
+  validate :username_is_allowed
+  
+  
+  private
+  
+  def username_is_allowed
+    if FORBIDDEN_USERNAMES.include?(username)
+      errors.add(:username, "has been restricted from use.")
+    end
+  end
 
 end
