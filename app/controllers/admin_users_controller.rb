1cd90ada6ecfcd83110fe5f20765754825d74f35
Faisal Hassan <faisal.hassanx@gmail.com>
2017-03-13 20:19:40 +0000

    Add action methods to admin users controller

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -1,13 +1,52 @@
 class AdminUsersController < ApplicationController
+  
+  layout 'admin'
+  
+  before_action :confirm_logged_in
+  
   def index
+    @admin_users = AdminUser.sorted
   end
 
   def new
+    @admin_user = AdminUser.new
+  end
+  
+  def create
+    @admin_user = AdminUser.new(admin_user_params)
+    if @admin_user.save
+      flash[:notice] = "Admin user created successfully"
+      redirect_to(admin_users_path)
+    else
+      render('new')
+    end
   end
 
   def edit
+    @admin_user = AdminUser.find(params[:id])
   end
 
   def delete
+    @admin_user = AdminUser.find(params[:id])
   end
+  
+  def destroy
+    @admin_user = AdminUser.find(params[:id])
+    @admin_user.destroy
+    flash[:notice] = "Admin user deleted successfully"
+    redirect_to(admin_users_path)
+  end
+  
+  private
+  
+  def admin_user_params
+    params.require(:admin_user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :username,
+      :password
+      )
+  end
+  
 end
