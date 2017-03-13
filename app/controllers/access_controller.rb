7289fe2587b3a65bf46c04726ce556ba103a70d6
Faisal Hassan <faisal.hassanx@gmail.com>
2017-03-13 01:45:10 +0000

    Add access controller actions and menu view

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -1,7 +1,19 @@
 class AccessController < ApplicationController
+  
   def menu
+    # display text and links
   end
 
   def login
+    # login form
   end
+  
+  def attempt_login
+    
+  end
+  
+  def logout
+    
+  end
+  
 end
