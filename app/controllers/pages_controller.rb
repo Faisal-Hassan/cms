b9de77e55ecfb3625efa1ce128d57405bf8fe9e6
Faisal Hassan <faisal.hassanx@gmail.com>
2017-03-17 02:15:18 +0000

    Remove subject id and page id from whitelist params

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -21,6 +21,7 @@ class PagesController < ApplicationController
   
   def create
     @page = Page.new(page_params)
+    @page.subject = @subject
     if @page.save
       flash[:notice] = "Page has been saved successfully"
       redirect_to(pages_path(:subject_id => @subject.id))
@@ -58,7 +59,7 @@ class PagesController < ApplicationController
   private
   
   def page_params
-    params.require(:page).permit(:subject_id, :name, :position, :visible, :permalink)
+    params.require(:page).permit(:name, :position, :visible, :permalink)
   end
   
   def find_subjects
--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -21,6 +21,7 @@ class SectionsController < ApplicationController
   
   def create
     @section = Section.new(section_params)
+    @section.page = @page
     if @section.save
       flash[:notice] = "Section has been created successfully"
       redirect_to(sections_path(:page_id => @page.id))
@@ -57,7 +58,7 @@ class SectionsController < ApplicationController
   private
   
   def section_params
-    params.require(:section).permit(:page_id, :name, :position, 
+    params.require(:section).permit(:name, :position, 
     :visible, :content_type, :content)
   end
   
